690b827f8a37114214cfe4bf2fe2a49edb3cf8c6
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-10 15:59:44 -0300

    And ex004.py

--- ex004.py
+++ ex004.py
@@ -1,3 +1,13 @@
 #https://docs.python.org/pt-br/3/library/stdtypes.html
 #https://pythonacademy.com.br/blog/tipos-de-variaveis-no-python
 
+print("Vamos saber os tipos da entra de dados:")
+a = input("Digite algo? ")
+print("Qual o tipo primitivo? ", type(a))
+print("Só tem espaços? ", a.isspace())
+print("É um número? ", a.isnumeric())
+print("É alfabético? ", a.isalpha())
+print("É alfanumérico? ", a.isalnum())
+print("Estão em maiúsculas? ", a.isupper())
+print("Estão em minúsculas? ", a.islower())
+print("Está capitalizada? ", a.istitle())
