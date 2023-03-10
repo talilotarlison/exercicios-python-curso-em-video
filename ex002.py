54d0f6084cc1b78605ca9ce028747e0d1d9d4fa5
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-10 10:39:21 -0300

    Update ex002.py

--- ex002.py
+++ ex002.py
@@ -1,3 +1,6 @@
+#https://pt.stackoverflow.com/questions/173939/gostaria-de-de-saber-qual-%C3%A9-a-fun%C3%A7%C3%A3o-do-na-linguagem-python-e-mais-especific
+
+
 #modelo 1
 nome = input("Digite seu nome");
 print ("Seu nome é %s " %nome)
@@ -11,3 +14,10 @@ print ("Seu nome é ", nome)
 
 nome = input("Digite seu nome");
 print ("Seu nome é {}" .format(nome))
+
+#modelo 4
+
+nome = input("Escreva seu nome: ")
+idade = input(f'{nome}, agora escreva sua idade: ')
+
+print(f'{nome}, {idade} - Identidade criada')
