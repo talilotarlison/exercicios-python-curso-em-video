dc121e073de5c550f9ff59e97998f097c19e530b
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-10 15:08:11 -0300

    Create
    Soma de dois numeros!

---
+++ ex003-02.py
@@ -0,0 +1,5 @@
+print("Vamos somar dois numeros")
+numero1 = int(input("Digite o primeiro numero "))
+numero2 = int(input("Digite o segundo numero "))
+total = numero1 + numero2
+print('O resultadode {} e {} e igual a {}' .format(numero1, numero2, total))
