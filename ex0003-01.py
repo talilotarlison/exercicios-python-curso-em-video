b83fc1c623aa684402666840252812edcbb41589
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-10 11:01:18 -0300

    Create

---
+++ ex0003-01.py
@@ -0,0 +1,11 @@
+acumulador = 0
+ciclos = 4 #numero de inputs digitado pelo usuário
+ 
+ 
+for i in range(ciclos):
+  print("Digite o valor: ")
+  numeros = float(input())
+  acumulador += numeros # o mesmo que dizer: acumulador = (acumulador + numeros)
+ 
+media = (acumulador/ciclos)
+print("Média dos valores informados: ", media)
