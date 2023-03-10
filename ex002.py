fa882aeccb6465801f98d80a7fb3f9d72fdc830b
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-10 10:36:32 -0300

    Create

---
+++ ex002.py
@@ -0,0 +1,13 @@
+#modelo 1
+nome = input("Digite seu nome");
+print ("Seu nome é %s " %nome)
+
+#modelo 2
+
+nome = input("Digite seu nome");
+print ("Seu nome é ", nome)
+
+#modelo 3
+
+nome = input("Digite seu nome");
+print ("Seu nome é {}" .format(nome))
