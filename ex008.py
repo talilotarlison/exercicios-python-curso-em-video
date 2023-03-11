91f4118fe32d2591f4d59a5b1a8ab2315d0a5fb5
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-11 09:38:54 -0300

    Create

---
+++ ex008.py
@@ -0,0 +1,6 @@
+print("converter Moeda --> Tranforma real em dolar")
+numero = float(input("Ecolha o valor em real que quer converter em dolar?"))
+
+dolar = numero * 0.19
+
+print("O valor R${} em Real, corresponde a ${} em dolar!" .format(numero,dolar))
