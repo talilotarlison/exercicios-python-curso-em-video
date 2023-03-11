0acf1fe046d05a2ad5c8c6cef7c1831d2813179d
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-11 09:24:34 -0300

    Create

---
+++ ex005.py
@@ -0,0 +1,7 @@
+print("Sucessor e antecessor de um numero")
+numero = float(input("Exolha um numero?"))
+
+s = numero + 1
+a = numero - 1
+
+print("Sucessor {} e antecessor{} do numero {}!" .format(s,a,numero ))
