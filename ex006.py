e5380252523f7a186557942c63b9ae166854395d
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-11 09:30:17 -0300

    Create

---
+++ ex006.py
@@ -0,0 +1,8 @@
+print("Sucessor e antecessor de um numero")
+numero = float(input("Ecolha um numero?"))
+
+d = numero * 2
+t = numero * 3
+r = numero ** (1/2)
+
+print("Dobro {}\n e o trioplo {}\n e a raiz quadradra {}!" .format(d,t, r ))
