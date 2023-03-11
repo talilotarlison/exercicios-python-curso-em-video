422ac84706251e63f41b4cd27a341980b2d0edc7
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-11 11:16:49 -0300

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -1,6 +1,19 @@
+#primeira meneira
+
 print("converter Moeda --> Tranforma R$ Real em $Dolar")
 numero = float(input("Ecolha o valor em  R$ real que quer converter em $ dolar?"))
 
 dolar = numero * 0.19
 
 print("O valor R${:.2f} em Real, corresponde a ${:.2f} em dolar!" .format(numero,dolar))
+
+
+#segunda meneira
+
+print("converter Moeda --> Tranforma R$ Real em $Dolar")
+numero = float(input("Ecolha o valor em  R$ real que quer converter em $ dolar?"))
+
+dolar = numero / 5.20
+
+print("O valor R${:.2f} em Real, corresponde a ${:.2f} em dolar!" .format(numero,dolar))
+
