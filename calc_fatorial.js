dfa3b5b01a74ec1a2b58f6b893e838ea76173ac3
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-16 16:49:30 -0300

    Create

---
+++ calc_fatorial.js
@@ -0,0 +1,33 @@
+var numero = prompt("Digite o numero fatorial: ")
+fatorial = numero
+for (var i = 1; i < numero; i++){
+  fatorial = fatorial * i  
+  
+} 
+console.log(`seu numero é ${fatorial}`)
+
+
+var numero = 10
+fatorial = numero
+for (var i = 1; i < numero; i++){
+  fatorial = fatorial * i  
+  
+} 
+console.log('seu numero é' + fatorial)
+
+// uso de switch no javascrip
+const expr = prompt()
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
+
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/switch
