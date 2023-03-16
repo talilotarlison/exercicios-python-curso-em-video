4a7b1e25d1602b16c1cfc864eb5179727e09cbb4
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-03-16 10:02:19 -0300

    Create

---
+++ ordenação_valores.py
@@ -0,0 +1,9 @@
+#ORDENAÇÃO SIMPLE DE UMA LISTA DE 2 VALORES
+
+numeros = [55,12]
+
+if numeros[0] > numeros[1]:
+    aux = numeros[0]
+    numeros[0] = numeros[1]
+    numeros[1] = aux
+    print(numeros)
