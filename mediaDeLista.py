e9cd705af39c1663a40d07978282e37331d7b3eb
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-06-06 10:30:20 -0300

    mediaDeLista.py

---
+++ mediaDeLista.py
@@ -0,0 +1,7 @@
+// Aqui está um exemplo de código em Python que calcula a média de uma lista de números.
+
+def media(lista):
+    return sum(lista) / len(lista)
+
+lista = [1, 2, 3, 4, 5]
+print(media(lista))
