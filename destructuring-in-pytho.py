264ed9c984c233b1499aa8ce3271d5ac54b6bede
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-05-18 22:09:51 -0300

    Create destructuring-in-pytho.py

---
+++ destructuring-in-pytho.py
@@ -0,0 +1,7 @@
+#https://blog.teclado.com/destructuring-in-python/
+#https://pt.stackoverflow.com/questions/76428/como-gerar-n%C3%BAmeros-aleat%C3%B3rios-em-python
+
+my_dict = {"name": "Bob", "age": 25}
+name, age = my_dict.values()  # "Bob", 25
+
+print(name, age)
